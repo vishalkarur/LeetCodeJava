61811824975aa97631c6ec3eaa16cc5f3116690c
Vishal karur <62991525+vishalkarur@users.noreply.github.com>
2022-01-06 20:00:21 +0530

    Time: 19 ms (14.91%), Space: 41.4 MB (16.87%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int ans = 0;
+        int a = x;
+        if(x<0) {
+            return false;
+        }
+        while(x!=0){
+            int rem = x % 10;
+            x = x / 10;
+            ans = ans * 10 + rem ;
+        }
+        if(a==ans) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
